d1bd2dcf4a2ce783e81259f852dc070034473689
OhMesch <mze5111@gmail.com>
2018-01-29 22:21:18 -0500

    1 - Times 2

---
+++ times2.m
@@ -0,0 +1,7 @@
+% Problem 1. Times 2 - START HERE
+function y = times2(x)
+  % Modify the line below so that the output y is twice the incoming value x
+  y = 2*x;
+  % After you fix the code, press the "Submit" button, and you're on your way.
+end
+
